533ccec59e3f9d68d8b1186c5c806befe5eebb6f
Riley Timlin <RTimlin@allseasonmovers.com>
2023-03-14 11:30:33 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Form from './components/Form';
+import Display from './components/Display';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Nav />
+      <Form />
+      <Display />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <form>
+        <input type="text"> </input>
+        <input type="submit"></input>
+        </form>
+    </div>
+  )
+}
+
+export default Form
